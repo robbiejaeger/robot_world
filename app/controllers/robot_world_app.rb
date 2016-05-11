a01458284748279b02fcf476b524443d2db9eaae
Robbie Jaeger <jaeger.rob@gmail.com>
2016-05-11 07:30:17 -0600

    Add all CRUD functionality

---
+++ app/controllers/robot_world_app.rb
@@ -0,0 +1,49 @@
+require 'models/robot_manager'
+
+class RobotWorldApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+  set :method_override, true
+
+  get '/' do
+    erb :index
+  end
+
+  get '/robots' do
+    @robots = robot_manager.all
+    erb :all
+  end
+
+  get '/robots/new' do
+    erb :new
+  end
+
+  post '/robots' do
+    robot_manager.create(params[:robot])
+    redirect '/robots'
+  end
+
+  get '/robots/:id' do |id|
+    @robot = robot_manager.find(id.to_i)
+    erb :show
+  end
+
+  get '/robots/:id/edit' do |id|
+    @robot = robot_manager.find(id.to_i)
+    erb :edit
+  end
+
+  put '/robots/:id' do |id|
+    robot_manager.update(id.to_i, params[:robot])
+    redirect "/robots/#{id}"
+  end
+
+  delete '/robots/:id' do |id|
+    robot_manager.destroy(id.to_i)
+    redirect '/robots'
+  end
+
+  def robot_manager
+    database = YAML::Store.new('db/robot_manager')
+    @robot_manager ||= RobotManager.new(database)
+  end
+end
---
+++ app/models/robot_manager.rb
@@ -0,0 +1,65 @@
+require 'yaml/store'
+require_relative 'robot'
+
+class RobotManager
+  attr_reader :database
+
+  def initialize(database)
+    @database = database
+  end
+
+  def create(robot)
+    database.transaction do
+      database['robots'] ||= []
+      database['total'] ||= 0
+      database['total'] += 1
+      database['robots'] << {
+        "id" => database['total'],
+        "name" => robot[:name],
+        "city" => robot[:city],
+        "state" => robot[:state],
+        "avatar" => "https://robohash.org/#{robot[:name]}.png",
+        "birthdate" => robot[:birthdate],
+        "date_hired" => robot[:date_hired],
+        "department" => robot[:department]
+      }
+    end
+  end
+
+  def raw_robots
+    database.transaction do
+      database['robots'] || []
+    end
+  end
+
+  def all
+    raw_robots.map{|robot_data| Robot.new(robot_data)}
+  end
+
+  def raw_robot(id)
+    raw_robots.find {|robot| robot["id"] == id}
+  end
+
+  def find(id)
+    Robot.new(raw_robot(id))
+  end
+
+  def update(id, robot)
+    database.transaction do
+      target_robot = database['robots'].find {|robot| robot["id"] == id}
+      target_robot["name"] = robot[:name]
+      target_robot["city"] = robot[:city]
+      target_robot["state"] = robot[:state]
+      target_robot["avatar"] = "https://robohash.org/#{robot[:name]}.png"
+      target_robot["birthdate"] = robot[:birthdate]
+      target_robot["date_hired"] = robot[:date_hired]
+      target_robot["department"] = robot[:department]
+    end
+  end
+
+  def destroy(id)
+    database.transaction do
+      database['robots'].delete_if {|robot| robot["id"] == id}
+    end
+  end
+end
